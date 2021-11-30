9df0485fa7d85111e47b58ee1a8900c8697ecbc3
NaveenMONK <naveenbalaji786@gmail.com>
2021-11-30 15:14:29 +0530

    Using method 1

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-// const up = document.querySelector(".up");
-// const down = document.querySelector(".down");
-// const reset = document.querySelector(".reset");
+const up = document.querySelector(".up");
+const down = document.querySelector(".down");
+const reset = document.querySelector(".reset");
 const count = document.querySelector(".count");
 const btn = document.querySelector(".button");
 
-// up.addEventListener("click", () => {
-  
-//   count.innerHTML++;
-//   count.style.color = "orange";
-// });
+up.addEventListener("click", () => {
+  count.innerHTML++;
+  count.style.color = "orange";
+});
 
-// down.addEventListener("click", () => {
+down.addEventListener("click", () => {
+  count.innerHTML--;
+  count.style.color = "pink";
+});
 
-//   count.innerHTML--;
-//   count.style.color = "pink";
-// });
+reset.addEventListener("click", () => {
+  count.innerHTML = 0;
+  count.style.color = "black";
+});
 
-// reset.addEventListener("click", () => {
-//   count.innerHTML = 0;
-//   count.style.color = "black";
-// });
+// // Method 2
 
-// Method 2 coming soon
+// btn.addEventListener("click",(e)=>{
+//     if(e.EventTarget.classList.contains("up")){
+//         count.innerHTML++;
 
-btn.addEventListener("click",(e)=>{
-    if(e.EventTarget.classList.contains("up")){
-        count.innerHTML++;
+//     }
 
-    }
-
-    
-
-})
+// })
