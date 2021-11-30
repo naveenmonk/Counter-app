ee3355e01bab010a3a0e53f11e72f3f5b1874b36
NaveenMONK <naveenbalaji786@gmail.com>
2021-11-30 14:56:35 +0530

    version 2.0

---
+++ script.js
@@ -0,0 +1,34 @@
+// const up = document.querySelector(".up");
+// const down = document.querySelector(".down");
+// const reset = document.querySelector(".reset");
+const count = document.querySelector(".count");
+const btn = document.querySelector(".button");
+
+// up.addEventListener("click", () => {
+  
+//   count.innerHTML++;
+//   count.style.color = "orange";
+// });
+
+// down.addEventListener("click", () => {
+
+//   count.innerHTML--;
+//   count.style.color = "pink";
+// });
+
+// reset.addEventListener("click", () => {
+//   count.innerHTML = 0;
+//   count.style.color = "black";
+// });
+
+// Method 2 coming soon
+
+btn.addEventListener("click",(e)=>{
+    if(e.EventTarget.classList.contains("up")){
+        count.innerHTML++;
+
+    }
+
+    
+
+})
